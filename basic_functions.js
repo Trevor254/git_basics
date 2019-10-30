2e462a23c195e47b933a0c12acfd54dc73c18c61
Trevor254 <trevoromondi607@gmail.com>
2019-10-30 12:25:00 +0300

    modify file

---
+++ basic_functions.js
@@ -0,0 +1,22 @@
+let num_arr = [1,2,3,4,5,6];
+
+function sumEven() {
+  let sum = 0;
+  num_arr.forEach((e, i) => {
+    if (e % 2 === 0) {
+      sum += e;
+    }
+  });
+  return sum;
+}
+// log out return value of function
+console.log(sumEven());
+
+function returnOdd() {
+  return num_arr.filter(e => !(e % 2 === 0));
+}
+
+console.log(returnOdd());
+
+//Contributor:Trevor254
+
